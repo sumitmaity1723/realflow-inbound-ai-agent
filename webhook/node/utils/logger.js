fcb7d98d046f5e3990d8493e00b800b9483a9b1e
sumitmaity1723 <sumitmaity1723@gmail.com>
2025-11-03 23:17:48 +0530

    Update logger.js

--- webhook/node/utils/logger.js
+++ webhook/node/utils/logger.js
@@ -1,1 +1,10 @@
+const fs = require('fs').promises;
+
+async function logToFile(data) {
+  const line = JSON.stringify(data) + '\n';
+  await fs.appendFile('call_logs.jsonl', line);
+  console.log("Saved:", data);
+}
+
+module.exports = { logToFile };
 
